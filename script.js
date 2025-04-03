c5784a86c09ea3b41cc860b17ec7b6809fd646da
Your Name <you@example.com>
2025-04-03 13:02:37 +0200

    d

--- script.js
+++ script.js
@@ -57,8 +57,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 function(printer, code) {
                     if (code !== 'OK') {
                         updateStatus('Failed to create printer object: ' + code, true);
+                        console.log('before open')
                         // Open url in a new tab
                         window.open('https://10.0.4.14/', '_blank');
+                        console.log('after open')
                         epos.disconnect();
                         return;
                     }
